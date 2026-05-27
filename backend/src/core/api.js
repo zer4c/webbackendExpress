d2ff6a7b67d93076778959e3434df4ea782af4a6
zer4c <pabloazurduy@gmail.com>
2026-05-26 22:13:46 -0400

    feat: added cors

--- backend/src/core/api.js
+++ backend/src/core/api.js
@@ -3,7 +3,8 @@ const todoItemRoutes = require('../modules/item/routes.js');
 const {errorMiddleware }= require('../middleware/error.js')
 
 const app = express();
-
+const cors = require('cors')
+app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json());
 
 app.use('/todolist/item', todoItemRoutes);
